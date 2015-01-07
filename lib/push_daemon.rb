69186d6a3ff532e41ecda55a4159265314790ef1
meng <meng.wang@rea-group.com>
2015-01-07 17:34:59 +0800

    extract abstract server object

--- lib/push_daemon.rb
+++ lib/push_daemon.rb
@@ -1,15 +1,15 @@
-require_relative 'server'
+require_relative 'app'
 require_relative 'worker'
 
 class PushDaemon
   def initialize
     @queue = Queue.new
-    @server = Server.new
+    @app = App.new(@queue)
     @worker = Worker.new
   end
 
   def start
     @worker.start(@queue)
-    @server.start(@queue)
+    @app.start
   end
 end
--- lib/server.rb
+++ lib/server.rb
@@ -1,33 +1,17 @@
-require "socket"
-require "json"
-
 class Server
   def initialize
     @socket = UDPSocket.new
   end
 
-  def start(queue)
-    bind
-    listen queue
+  def bind port
+    @socket.bind("0.0.0.0", port)
   end
 
-  def bind
-    @socket.bind("0.0.0.0", 6889)
+  def receive
+    @socket.recvfrom(4096)
   end
 
-  def listen(queue)
-    while data = @socket.recvfrom(4096)
-      case data[0].split.first
-        when "PING"
-          @socket.send("PONG", 0, data[1][3], data[1][1])
-        when "SEND"
-          data[0][5..-1].match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
-          json = JSON.generate({
-                                   "registration_ids" => [$1],
-                                   "data" => {"alert" => $2}
-                               })
-          queue << json
-      end
-    end
+  def send(message, address, port)
+    @socket.send(message, 0, address, port)
   end
 end
