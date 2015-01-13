f4ebe77df804aff067103dae6f9e5df8c36125fd
meng <meng.wang@rea-group.com>
2015-01-13 17:42:45 +0800

    remove switch case

--- lib/jobs/factory.rb
+++ lib/jobs/factory.rb
@@ -3,11 +3,7 @@ require_relative "send"
 
 module Jobs
   def self.factory data, server
-    case data[0].split.first
-      when "PING"
-        Jobs::Ping.new(server, data)
-      when "SEND"
-        Jobs::Send.new(server, data)
-    end
+      job_type = data[0].split.first.capitalize
+      const_get(job_type).new(server, data)
   end
 end
